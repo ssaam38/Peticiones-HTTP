28a134fe64e3da448b2ae6e974c85d10cd043ef9
Samuel <samuel.alarcon177@gmail.com>
2022-12-01 13:08:39 +0100

    primer commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//import * as CRUD from "./js/crud-provider";
+//CRUD.getUsuario(2).then(console.log);
+
+import { init } from "./js/archivos-page";
+init();
+
+//CRUD.crearUsuario({
+//    name: 'Fernando',
+//    job: 'Carpintero'
+
+//}).then(console.log);
+
+//CRUD.actualizarUsuario(1,{
+//name: 'Melissa',
+//job: 'Profesora'
+//}).then(console.log);
+
+//CRUD.borrarUsuario(1).then(console.log);
+
+//import { init } from './js/usuarios-page';
+//init();
+//import { obtenerUsuarios } from './js/http-provider';
+//obtenerUsuarios().then(console.log);
+
+
